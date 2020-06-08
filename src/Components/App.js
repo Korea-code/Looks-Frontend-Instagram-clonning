7012b24b3460d156d1ade3e4a45fb1a9824a23ce
Korea-code <rlawlgy93@gmail.com>
2020-06-08 15:32:15 +0900

    Authentication

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,26 @@
 import React from "react";
+import { gql } from "apollo-boost";
 import { ThemeProvider } from "styled-components";
+import { useQuery } from "react-apollo-hooks";
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
 import AppRouter from "./Router";
 
-export default () => (
-  <ThemeProvider theme={Theme}>
-    <>
+const QUERY = gql`
+  {
+    isLoggedIn @client
+  }
+`;
+
+export default () => {
+  const {
+    data: { isLoggedIn }
+  } = useQuery(QUERY);
+
+  return (
+    <ThemeProvider theme={Theme}>
       <GlobalStyles />
-      <AppRouter isLoggedIn={!false} />
-    </>
-  </ThemeProvider>
-);
+      <AppRouter isLoggedIn={isLoggedIn} />
+    </ThemeProvider>
+  );
+};
