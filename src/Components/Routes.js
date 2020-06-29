43cb85321f66df8f307687c9e7e49b909502ce70
Korea-code <rlawlgy93@gmail.com>
2020-06-29 23:32:56 +0900

    redirect * to /

--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Auth from "../Routes/Auth";
 import Feed from "../Routes/Feed";
 import Explore from "../Routes/Explore";
@@ -19,11 +19,13 @@ const LoggedInRoutes = () => (
     <Route path="/editProfile" component={EditProfile} />
     <Route path="/post/:postId" component={Post} />
     <Route path="/:username" component={Profile} />
+    <Redirect from="/*" to="/" />
   </Switch>
 );
 const LoggedOutRoutes = () => (
   <Switch>
     <Route exact path="/" component={Auth} />
+    <Redirect from="/*" to="/" />
   </Switch>
 );
 const AppRouter = ({ isLoggedIn }) =>
